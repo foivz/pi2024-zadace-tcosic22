9d40c8be0ad4831f9a4931c1e64cb5175530da9f
Tomislav Cosic <tcosic22@student.foi.hr>
2024-05-28 01:11:05 +0200

    Kreirana klasa DB sa pristupnim podacima za bazu te metodama za uspostavljanje veze, zatvaranje veze, dohvaćanje čitača te izvršavanje sql komande.

--- Software/Zadaca3/DBLayer/DB.cs
+++ Software/Zadaca3/DBLayer/DB.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DBLayer
 {
-    public class Class1
+    public class DB
     {
+        private static string _connectionString = @"Data Source=31.147.206.65\PISERVER,1433; Initial Catalog=PI2324_tcosic22_DB;
+        User=PI2324_tcosic22_User; Password=-t0nGBJl";
+        private static SqlConnection _connection;
+
+        public static void OpenConnection()
+        {
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            if (_connection.State != System.Data.ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+
+        public static SqlDataReader GetDataReader(string query)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            return command.ExecuteReader();
+        }
+
+        public static int ExecuteCommand(string sqlCommand)
+        {
+            SqlCommand command = new SqlCommand(sqlCommand, _connection);
+            return command.ExecuteNonQuery();
+        }
     }
 }
